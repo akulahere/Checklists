a1830bb2be27ba81410644231c2928c3b670968f
akula <akulinind@gmail.com>
2022-06-20 10:14:13 +0300

    Initial Commit

---
+++ CheckList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CheckList
+//
+//  Created by Dmitro Akulinin on 20.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
