7800a4b9914b494321572862c4e27cce319f9add
akula <akulinind@gmail.com>
2022-06-21 18:01:32 +0300

    feat: implement Add item view

--- CheckList/AddItemViewController.swift
+++ CheckList/AddItemViewController.swift
@@ -7,19 +7,53 @@
 
 import UIKit
 
-class AddItemViewController: UITableViewController {
+class AddItemViewController: UITableViewController, UITextFieldDelegate {
+  @IBOutlet weak var textField: UITextField!
+  
+  @IBOutlet weak var doneBarButton: UIBarButtonItem!
   override func viewDidLoad() {
     super.viewDidLoad()
     navigationItem.largeTitleDisplayMode = .never
   }
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    textField.becomeFirstResponder()
+  }
   
-  //MARK: - Actions
+  // MARK: - Actions
   @IBAction func cancel() {
     navigationController?.popViewController(animated: true)
   }
   
   @IBAction func done() {
+    print("\(textField.text!)")
     navigationController?.popViewController(animated: true)
   }
+  
+  // MARK: - Table View Delegates
+  override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+    return nil
+  }
+  
+  // MARK: - Text Field Delegates
+  func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+    let oldText = textField.text!
+    
+    let stringRange = Range(range, in: oldText)!
+    let newText = oldText.replacingCharacters(in: stringRange, with: string)
+    if newText.isEmpty {
+      doneBarButton.isEnabled = false
+    } else {
+      doneBarButton.isEnabled = true
+    }
+    return true
+  }
+  
+  func textFieldShouldClear(_ textField: UITextField) -> Bool {
+    doneBarButton.isEnabled = false
+    return true
+  }
+
+  
 }
 
